cfa47086aaac37176eaea0eec01ea0ecb1604696
Dzmitry Rekuts <dzmitry_rekuts@epam.com>
2015-12-07 15:57:23 +0300

    NewtonMethod.

---
+++ ConsoleAppRootFinding/Program.cs
@@ -0,0 +1,41 @@
+﻿using NewtonMethodLogik;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppRootFinding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number;
+            double precision;
+            double power;
+
+
+            Console.WriteLine("Enter your number");
+            Double.TryParse(Console.ReadLine(), out number);
+            Console.WriteLine("Enter the desired power");
+            Double.TryParse(Console.ReadLine(), out power);
+            double reversPower = 1 / power;
+            Console.WriteLine("Enter the required precision");
+            Double.TryParse(Console.ReadLine(), out precision);
+
+            double xNewton = RootFinding.NewtonMetode(number, precision, power);
+            Console.WriteLine("NewtonMetod Result : x = " + xNewton + "()");
+            double xPow = Math.Pow(number, reversPower);
+            Console.WriteLine("Math.Root Result : x = " + xPow);
+
+
+            if (xNewton == xPow)
+            {
+                Console.WriteLine(xNewton + " = " + xPow);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ NewtonMethodLogik/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewtonMethodLogik
+{
+    public class RootFinding
+    {
+
+        public static double NewtonMetode(double number, double precision, double power)
+        {
+            double x = number/power;
+
+            for (;;)
+            {
+                double nx = ((power - 1) * x + number / Math.Pow(x, power - 1)) / power;
+                if (Math.Abs(x - nx) < precision)
+                {
+                    break;
+                }
+                x = nx;
+            }
+
+
+            return x;
+        }
+    }
+}
